6afd0b0d4edb077459986982aba712c5f2f6e4c1
Tim.Liu <memo20xx@sina.com>
2020-01-17 09:38:18 +0800

    01HelloWorld.添加01项目

---
+++ 01_HelloWorld/01HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //MainWindow w;
+    //w.show();
+    //QLabel *label = new QLabel("Hello,World.");
+    //label->show();
+    QLabel label("Hello,world.");
+    label.show();
+    return a.exec();
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Qt5Tutorial
+
+本代码仓库用于管理和同步在学习Qt过程中涉及到的实例代码。  
+
+除特别说明外，本代码库对应的 Qt 版本均为 `5.14.0 (MinGW 7.3.0 64bit)` 。  
+
+代码库地址： <https://github.com/memo20xx/Qt5Tutorial>  
+
+___
+## 参考资料
+
+[Qt 学习之路 2 | DevBean Tech World](https://www.devbean.net/category/qt-study-road-2)  
